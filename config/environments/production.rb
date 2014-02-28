527e567cafaf6468183c09f8fdee1d239030d6e5
russell-cook <russ.cook@gmail.com>
2014-02-28 11:37:22 -0800

    fix copy-paste error in production.rb (mistakenly copied BudgetInitiative::Application)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-BudgetInitiative::Application.configure do
+ItSpend::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
