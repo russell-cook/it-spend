4fe86c6f9183f15bdc2dfb206c68023404f0247e
russell-cook <russ.cook@gmail.com>
2014-03-05 15:49:59 -0800

    update form for Eric review

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -85,6 +85,6 @@ class SurveysController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def survey_params
-      params.require(:survey).permit(:completing_name, :completing_title, :completing_email, :contact_name, :contact_title, :contact_email, :num_servers, :type_servers, :num_locations, :colocated, :dedicated, :state_owned, :state_owned_sqft, :leased, :leased_sqft, :access_control, :ups, :envir_ctrl, :generator, :fire, :data_server, :data_san, :total_capacity, :used_capacity, :backup, :local_backup, :central_backup, :freq_backup, :freq_backup_other, :who_manages, :who_manages_other, :hard_maint, :hard_maint_amt, :soft_maint, :soft_maint_amt, :contractor_support, :contractor_support_amt)
+      params.require(:survey).permit(:completing_name, :completing_title, :completing_email, :contact_name, :contact_title, :contact_email, :num_servers, :type_servers, :num_locations, :colocated, :dedicated, :state_owned, :state_owned_sqft, :leased, :leased_sqft, :access_control, :ups, :envir_ctrl, :generator, :fire, :data_server, :data_san, :total_capacity, :used_capacity, :backup, :local_backup, :central_backup, :freq_backup, :freq_backup_other, :who_manages, :who_manages_other, :hard_maint, :hard_maint_amt, :soft_maint, :soft_maint_amt, :contractor_support, :contractor_support_amt, :fte1, :fte1_prcnt, :fte2, :fte2_prcnt, :fte3, :fte3_prcnt, :fte4, :fte4_prcnt, :fte5, :fte5_prcnt, :fte6, :fte6_prcnt)
     end
 end
